5445e80e58ca4704b5730b7843833f0f3bc2538d
yashdubey <yash@headstrt.com>
2020-09-17 01:12:27 +0530

    Updates

---
+++ precache-manifest.683319ec011c0d741f2fdcca75815b49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4e9c18f81812b9164236859fa31484",
+    "url": "/whynotcalculator/index.html"
+  },
+  {
+    "revision": "dda9d5df23eb3e8d0ac2",
+    "url": "/whynotcalculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "094dc2bdccf2deeb7427",
+    "url": "/whynotcalculator/static/js/2.e2f82d03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whynotcalculator/static/js/2.e2f82d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda9d5df23eb3e8d0ac2",
+    "url": "/whynotcalculator/static/js/main.aa310915.chunk.js"
+  },
+  {
+    "revision": "d86cd8d8544b49b54702",
+    "url": "/whynotcalculator/static/js/runtime-main.ec10ccfe.js"
+  }
+]);
